d83b837740195fd98ca849a4be3a6ccc8e88c4bd
JovanyAldair <azevedojovani90@gmail.com>
2023-11-18 08:08:14 +0100

    app.test documented

--- test/app.test.js
+++ test/app.test.js
@@ -1,24 +1,27 @@
 /*
-Test the GET/ routes
+This file contains the tests of the GET/ routes
+It expects 200 OK
 */
 
-const request = require('supertest');
-const app = require('../app'); // the express server
+const request = require('supertest')
+const app = require('../app')
 
+//testing GET route /
 describe("GET /", () => {
   it("responds with a json message", function(done) {
     request(app)
       .get("/")
       .expect('Content-Type', /json/)
-      .expect(200, done);
+      .expect(200, done)
   });
 });
 
+//testing GET route /download
 describe("GET /download", () => {
   it("responds with a file download", function(done) {
     request(app)
       .get("/download")
       .expect('Content-Type', 'application/pdf')
-      .expect(200, done);
+      .expect(200, done)
   });
 });
